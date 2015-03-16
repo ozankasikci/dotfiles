311aa16378d98ee3ce305034e2ddd6b614f3a00d
Ozan Kasikci <ozan.kasikci@metglobal.com>
2015-03-16 09:55:16 +0200

    Added vimrc.bundles.local

--- makeLinks.sh
+++ makeLinks.sh
@@ -2,7 +2,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc tmux.conf ideavimrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="zshrc tmux.conf ideavimrc oh-my-zsh vimrc.bundles.local"    # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
